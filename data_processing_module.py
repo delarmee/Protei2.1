2058abc472985474b84d89f976e16a0c3bdb49f8
delarmee <raskopina.nastia@yandex.ru>
2024-01-09 08:57:08 +0300

    Нелинейная регрессия

--- data_processing_module.py
+++ data_processing_module.py
@@ -5,6 +5,8 @@ def load_and_process_data(file_path):
     data['время'] = pd.to_datetime(data['время'], format='%H:%M:%S').dt.hour
     return data
 
+# Определяем нижний и верхний пороги для удаления выбросов с использованием квантилей.
+# Используем эти пороги для удаления выбросов из  набора данных.(lower_quantile=0.05, upper_quantile=0.95)
 def filter_outliers(data, lower_quantile=0.05, upper_quantile=0.95):
     lower_bound = data['количество_сообщений'].quantile(lower_quantile)
     upper_bound = data['количество_сообщений'].quantile(upper_quantile)
